29a99b0e1819381072fc73a400e46f211d0c8b9b
Ross <rdaly525@stanford.edu>
2017-06-08 18:50:56 -0700

    First commit

---
+++ sim.py
@@ -0,0 +1,49 @@
+import random
+import math
+import cmath as cm
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import heapq
+import sklearn.decomposition as sc
+from sklearn.utils.extmath import randomized_svd
+from math import sqrt
+from scipy.linalg import eig
+from PIL import Image
+import scipy.misc
+
+if __name__ == "__main__":
+
+
+  #Use regularization: reg(x) = 1-2x**2 + x**4
+  def bitfield(i,N):
+    bits = [int(digit) for digit in bin(i)[2:]]
+    for i in range(N-len(bits)):
+      bits.insert(0,0)
+    #change to -1 to 0
+    return [bits[j]*2-1 for j in range(N)]
+
+  def lutN(N,sigma):
+    def norm(u):
+      s2 = sigma**2
+      front = math.exp(.792*N)*(((2*math.pi)**N)*s2)**(-0.5)
+      def f(x):
+        l2 = sum([(x[i]-u[i])**2 for i in range(N)])
+        return front * math.exp(-l2/(2.0*s2))
+      return f
+
+
+    def lut(init):
+      norms = [norm(bitfield(i,N)) for i in range(2**N)]
+      def run(x):
+        assert len(x) == N
+        ns = [norms[i](x) for i in range(2**N)]
+        vals = [norms[i](x)*init[i] for i in range(2**N)]
+        return sum(vals)
+      return run
+    return lut
+
+  #l = lut([np.random.uniform(-1,1) for i in range(8)])
+  for n in range(1,8):
+    l = lutN(n,1)([1 for i in range(2**n)])
+    print n, l([-1 for i in range(n)])
