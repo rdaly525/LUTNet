c4092088026cdf39dc7406436f8f1549c8902118
Michael Mara <mmara@Michaels-MacBook-Pro.local>
2017-08-03 17:27:49 -0700

    Commented common.py

--- common.py
+++ common.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 def flatten_list(l):
+  """ Single-level flatten """
   return [item for sublist in l for item in sublist]
 
-#phs=placeholders
-#curs=current values
 def make_feed_dict(phs, curs,thresh=0,prob=1):
+  """
+  phs=placeholders
+  curs=current values
+  """
   assert len(phs) == len(curs)
   def quant(x):
     if (x >=thresh and np.random.uniform() < prob):
@@ -23,6 +26,13 @@ def make_feed_dict(phs, curs,thresh=0,prob=1):
   return feed_dict
 
 def bitstr(i,N=None):
+  """ Convert integer i into string binary representation
+      Optionally pads to N binary digits.
+      String starts at LSB.
+
+      Examples: bitstr(18) == "01001"
+                bitstr(18,8) == "01001000"
+  """
   ret = bin(i)[2:][::-1]
   if N:
     for _ in range(N-len(ret)):
@@ -30,11 +40,17 @@ def bitstr(i,N=None):
   return ret
 
 def bitfield(i,N):
+  """ Convert integer i into binary representation then converts bits into {-1.0,1.0}
+      Optionally pads to N binary digits.
+      Index 0 is LSB.
+  """ 
   bits = bitstr(i,N)
   bits = [int(digit)*2-1 for digit in bits]
   return np.array(bits).astype(float)
 
+
 def scaleto11(val,check=True):
+  """Scales From [0,1] to [-1,1]"""
   if type(val) is not list and type(val) is not np.ndarray:
     val = [val]
   if type(val) is list:
@@ -44,6 +60,7 @@ def scaleto11(val,check=True):
   return val*2-1
 
 def scaleto01(val,check=True):
+  """Scales From [-1,1] to [0,1]"""
   if type(val) is not list and type(val) is not np.ndarray:
     val = [val]
   if type(val) is list:
